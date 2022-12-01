14bd58a887e32aff1df8cae85c703cddd102c9b0
Vasil Stoyanov <vasil@codebeez.nl>
2022-12-01 16:39:26 +0100

    implement fifth challenge
    Signed-off-by: Vasil Stoyanov <vasil@codebeez.nl>

--- challenges/5_logging_and_monitoring/log_messages.py
+++ challenges/5_logging_and_monitoring/log_messages.py
@@ -1,16 +1,26 @@
-import asyncio
+import asyncio as io
 import datetime
 
 
 # implement an async "log" function that passes send_message to asyncio.ensure_future
+def log(message):
+    task = io.create_task(send_message(message))
+    io.ensure_future(task)
 
 
 async def send_message(message):
-    await asyncio.sleep(1)
+    await io.sleep(1)
     print(message)
 
 
-def send_some_messages():
+async def send_some_messages():
     for message in ["first", "second", "third", "etc"]:
         print(f"log {datetime.datetime.now()} : {message}")
         log(message)
+
+    # sleed for a bit so the background tasks also have time to complete
+    await io.sleep(2)
+
+
+if __name__ == "__main__":
+    io.run(send_some_messages())
--- challenges/5_logging_and_monitoring/main.py
+++ challenges/5_logging_and_monitoring/main.py
@@ -1,32 +1,38 @@
-import asyncio
+import asyncio as io
 import aiomonitor
 from aiohttp import web
-import nest_asyncio
 
-nest_asyncio.apply()
 
-# Simple handler that returns response after 100s
+def log(message, loop):
+    task = io.create_task(send_message(message))
+    io.ensure_future(task, loop=loop)
+
+
+async def send_message(message):
+    await io.sleep(100)
+    print(message)
+
+
 async def simple(request):
     loop = request.app.loop
 
-    print("Start sleeping")
-    await asyncio.sleep(10, loop=loop)
-    print("Finished sleeping")
+    log("hello there", loop=loop)
+    await io.sleep(10, loop=loop)
     return web.Response(text="Simple answer")
 
 
-loop = asyncio.get_event_loop()
+loop = io.get_event_loop()
 host, port = "0.0.0.0", 8080
 
 locals_ = {"port": port, "host": host}
 
 # init monitor just before run_app
 with aiomonitor.start_monitor(loop=loop, locals=locals_):
-    loop = asyncio.get_event_loop()
+    loop = io.get_event_loop()
 
     # create application and register route
     app = web.Application()
     app.router.add_get("/simple", simple)
 
     # run application with built-in aiohttp run_app function
-    web.run_app(app, port=port, host=host)
+    web.run_app(app, port=port, host=host, loop=loop)
